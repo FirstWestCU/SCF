8edc23cedd4e4896001dad7ea6e919a3bd165d9f
Kevin Anderson <kevinford.anderson@gmail.com>
2014-07-11 16:45:10 -0600

    Add Classes and UserService
    Added the User Service with additional supporting classes for Users.
    Added a reference to BCrypt.dll. DLL added to References folder outside
    of project.

---
+++ SCF/Services/UserService.asmx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script;
+using System.Web.Script.Services;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Coop
+{
+    /// <summary>
+    /// Summary description for UserService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class UserService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public User CreateUser(string name, string email, string pass, int creditUnion)
+        {
+            int newUserID = 0;
+            pass = BCrypt.HashPassword(pass, BCrypt.GenerateSalt(10));      //Change 10 to increase hash rounds
+            string hash = HashGenerator.GenerateHash(email);
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SCF"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("InsertUser", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Hash", hash);
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.Parameters.AddWithValue("@Pass", pass);
+                    cmd.Parameters.AddWithValue("@CreditUnionID", creditUnion);
+                    conn.Open();
+                    newUserID = Convert.ToInt32(cmd.ExecuteScalar());
+                    conn.Close();
+                }
+            }
+
+            User newUser = new User();
+            newUser.LoadDetails(newUserID);
+            return newUser;
+        }
+    }
+}
